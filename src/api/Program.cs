6cdbe731888f5eea3f488ad06de88dd4b3e4afb8
Dimojiaku Obinna Anthony <dimojiakuo@accessbankplc.com>
2026-01-20 00:48:18 +0100

    swagger-re

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -149,11 +149,11 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 var app = builder.Build();
 
 // Configure middleware pipeline
-if (!app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
